cb77776defc49b4c2f7e5258a12e9094a531eb67
tihonhomyshen <lilo.frich@gmail.com>
2023-07-21 16:01:06 +0300

    main-page: bg downl, arrows works, bg not change

--- src/index.js
+++ src/index.js
@@ -1,4 +1,38 @@
-const pager = document.querySelector('.slider-controls')
-const activePage = document.getElementsByClassName('page')
+const pages = document.querySelectorAll('.page')
+const page = document.querySelector('.page')
 
-console.log(pager, activePage);
+const background = document.querySelector('.main-page')
+const currentBackground = "src/images/main-page/bg-images/bg-image"
+
+const currentPathBackground = window.getComputedStyle(background).getPropertyValue('background-image');
+
+const lastSlashIndex = currentPathBackground.lastIndexOf('/');
+const trimmedPath = currentPathBackground.substring(0, lastSlashIndex) + '/';
+
+console.log(currentPathBackground, trimmedPath);
+
+const leftArrowMP = document.querySelector('.arrow-left')
+const rightArrowMP = document.querySelector('.arrow-right')
+
+rightArrowMP.addEventListener('click', () => {
+    let activePageIndex = Array.from(pages).findIndex(page => page.classList.contains('active'));
+    if (activePageIndex >= 0){
+        pages[activePageIndex].classList.remove('active');
+        let nextPageIndex = (activePageIndex + 1) % pages.length;
+        pages[nextPageIndex].classList.add('active');
+        background.style.backgroundImage = `(${currentBackground}${activePageIndex})`;
+    }
+})
+
+leftArrowMP.addEventListener('click', () =>{
+    let activePageIndex = Array.from(pages).findIndex(page => page.classList.contains('active'));
+    if (activePageIndex >= 0){
+        pages[activePageIndex].classList.remove('active');
+        let nextPageIndex = activePageIndex - 1;
+        if (nextPageIndex < 0 ) {
+            nextPageIndex = pages.length - 1;
+        }
+        pages[nextPageIndex].classList.add('active');
+        console.log(activePageIndex, nextPageIndex);
+    }
+})
